724e548ceb19120b5438efc6599faf1356bd1382
Aliza Khan <124785215+AlizaKhan3@users.noreply.github.com>
2024-03-19 01:10:34 +0500

    Updated example of Multilevel inheritance

--- TestInheritance.java
+++ TestInheritance.java
@@ -3,15 +3,52 @@
 
 //Single Inheritance Example
 class Animal{  
-void eat(){System.out.println("eating...");}  
+    void eat(){
+       System.out.println("eating...");
+    }  
 }  
 class Dog extends Animal{  
-void bark(){System.out.println("barking...");}  
+    void bark(){System.out.println("barking...");
+    }  
 }  
+
 class TestInheritance{  
-public static void main(String args[]){  
-Dog d=new Dog();  
-d.bark();  
-d.eat();  
-}} 
+    public static void main(String args[]){  
+    Dog d=new Dog();  
+    d.bark();  
+    d.eat();  
+    }
+} 
+
+// --------------------------------------------------------------
+
+//Multilevel Inheritance Example (contains more than one class)
+
+class Teacher{                        //Base Class/Super Class
+    public void speaking (){
+        System.out.println("Teacher announced the marks of quiz!");
+    }
+}
+//Single level class
+class Students extends Teacher{       //Child class 1
+    public void listening (){
+        System.out.println("Not satisfied with marks");
+    }
+}
+//multilevel class
+class Parents extends Students{       //Child class 2
+    public void reacting(){
+        System.out.println("You need to work hard!");
+    }
+}
+
+public class TestInheritance{
+    public static void main (String args[]){
+        Parents p = new Parents();
+        p.speaking();
+        p.listening();
+        p.reacting();
+    }
+}
+
 
