7d0d7c56d169aa6cfd317659eb14d35c84136c5a
Aliza Khan <124785215+AlizaKhan3@users.noreply.github.com>
2024-03-18 16:59:36 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,42 @@
+// Polymorphism in Java poly-->many & morphism-->different forms
+// Polymorphism basically means that; in a same class having many different functions having diferent purposes but of same name is called Polymorphism or this kind of implementation 
+// refers to "Over Loading".
+
+// eg: code of Polymorphism
+class Student {
+    String name;
+    int age;
+    double SeatNumber;
+    
+    Student (){
+        //no-arg constructor if not created then runs as default constructor
+    }
+    
+    //Creating 3 functions of same name below (Implementing polymorphism)
+    public void PrintInfo(String name){       
+        System.out.println(name);
+    }
+    
+    public void PrintInfo(double SeatNumber){
+        System.out.println(SeatNumber);
+    }
+    
+     public void PrintInfo(int age){
+        System.out.println(age);
+    }
+    public void PrintInfo(String name, int age, double SeatNumber){
+        System.out.println(name + age + SeatNumber);
+   
+    }
+}
+
+public class Main{
+    public static void main (String args[]){
+        Student s1 = new Student();                      //Created an object
+        s1.name = "Aliza Khan";
+        s1.age = 18;
+        s1.SeatNumber = 6017;
+        
+        s1.PrintInfo("Students Data: Name --> " + s1.name + " ; Age --> " + s1.age + " ; Seat# --> " + s1.SeatNumber);
+    }
+}
